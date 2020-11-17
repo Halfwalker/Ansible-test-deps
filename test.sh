8ec79ce15181edf2f9ce6836accf645b3c01873d
Dean Carpenter <deano-github@areyes.com>
2020-11-17 09:32:04 -0500

    Fixed passing parameters to ansible-playbook

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ANSIBLE_STDOUT_CALLBACK=minimal ansible-playbook -i hosts test.yml $@ -- | egrep -v "{|}"
+ANSIBLE_STDOUT_CALLBACK=minimal ansible-playbook -i hosts test.yml $@ | egrep -v "{|}"
